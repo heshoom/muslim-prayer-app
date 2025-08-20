202a07c43bfa423bc2c471131ce33b8c67823cdd
heshoom <heshoom@users.noreply.github.com>
2025-08-20 02:29:33 -0400

    Attempt 6

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,17 +1,13 @@
+#
+//  ci_post_clone.sh
+//  IslamicPro
+//
+//  Created by Hesham on 8/20/25.
+//
+
 #!/bin/sh
 set -e
-
-# ci_post_clone.sh
-# IslamicPro
-# Created by Hesham on 8/20/25
-
 echo ">>> Cleaning and reinstalling Pods"
-cd frontend/ios || exit 1
-
-rm -rf Pods Podfile.lock
-
-# Install CocoaPods (user install)
+cd frontend/ios
 gem install cocoapods --user-install
-
-# Install pods with repo update and verbose logging
 pod install --repo-update --verbose
